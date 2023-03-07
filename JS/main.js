243b4a7afdb96efda29fdc67746baab1f476d251
YoussefHlem <80886022+HlemoXGamer@users.noreply.github.com>
2023-03-07 09:32:59 -0800

    Ohayo

---
+++ JS/main.js
@@ -0,0 +1,23 @@
+// Header
+
+// Bars
+let bars = document.querySelector(".bottom-head-bars");
+let menu = document.querySelector(".bottom-head-links");
+bars.addEventListener("click", () => {
+  menu.classList.toggle("active");
+});
+// Bars
+
+// Magnifier
+let magnifier = document.querySelector(".magnifier");
+let searchInput = document.getElementById("search-input");
+let categories = document.querySelector(".categories");
+magnifier.addEventListener("click", () => {
+  searchInput.classList.toggle("active");
+});
+// Magnifier
+
+// Cart
+// Cart
+
+// Header
